79f968cdd64fbb45b7eaf4d38dbcd98116d0ca6b
bulo45ka <bulo45ka@gmail.com>
2025-05-11 19:01:39 +0400

    Update AboutFragment.java

--- app/src/main/java/com/example/squadmaps/AboutFragment.java
+++ app/src/main/java/com/example/squadmaps/AboutFragment.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class AboutFragment extends Fragment {
+    /*пустой конструктор. он требуется согласно документации андройда.*/
     public AboutFragment() {
         // Required empty public constructor
     }
+    
+    /*метод для создания экзмепляра фрагмента.*/
     public static AboutFragment newInstance(String param1, String param2) {
         AboutFragment fragment = new AboutFragment();
         Bundle args = new Bundle();
@@ -19,11 +22,13 @@ public class AboutFragment extends Fragment {
         return fragment;
     }
 
+    /*метод для создания фрагмента.*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /*метод для создания макета для фрагмента.*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
