0fff29ff379791131fe915998775f3fb2d7b2bab
bulo45ka <bulo45ka@gmail.com>
2025-05-12 02:39:15 +0400

    Update ShareFragment.java

--- app/src/main/java/com/example/squadmaps/ShareFragment.java
+++ app/src/main/java/com/example/squadmaps/ShareFragment.java
@@ -8,11 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/*класс для передачи фрагмента.*/
 public class ShareFragment extends Fragment {
 
     public ShareFragment() {
-        // Required empty public constructor
+        // необходимый пустой конструктор.
     }
+    /*метод для создания экземпляра фрагмента.
+	входные значения: параметр 1, параметр 2.
+  	возвращаемые значения: новый экземпляр.*/
     public static ShareFragment newInstance(String param1, String param2) {
         ShareFragment fragment = new ShareFragment();
         Bundle args = new Bundle();
@@ -21,11 +25,16 @@ public class ShareFragment extends Fragment {
     }
 
     @Override
+    /*создание фрагмента.
+	входные значения: сохранненый статус фрагмента.
+  	возвращаемые значения: - .*/
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
+    /*вью верчу я на хую. спать хочется :х*/
+    /*НЕ ЗАБУДЬ УДАЛИТЬ ЭТО ВСЕ ТОЛЬКО САВЕЛЬЕВ НЕ ОДОБРИТ*/
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_share, container, false);
     }
