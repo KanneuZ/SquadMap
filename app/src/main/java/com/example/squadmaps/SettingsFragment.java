a18d8d073e68729663d1ed08411c70eb940b62dd
bulo45ka <bulo45ka@gmail.com>
2025-05-28 18:23:13 +0400

    Update SettingsFragment.java

--- app/src/main/java/com/example/squadmaps/SettingsFragment.java
+++ app/src/main/java/com/example/squadmaps/SettingsFragment.java
@@ -65,7 +65,7 @@ public class SettingsFragment extends Fragment {
     }
 
     @Override
-    /*блять*/
+    /*ааАаАаАаАаАааА*/
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
